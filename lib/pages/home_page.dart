17dded764083f12cbcfbe2cf554b2cdf74f54135
koarkarphyoe <koarkarphyoe@gmail.com>
2022-05-14 08:21:50 +0630

    Apply dispose function in page level widget from _bloc

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -31,6 +31,12 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
